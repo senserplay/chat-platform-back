cf7066b8ef392802496b80f7e05fa422023c3225
alexandrbelyanin <a.belyanin@neiro.com>
2025-05-28 01:31:29 +0300

    Added statistics setter

--- src/infrastructure/redis/storage/statistic_storage.py
+++ src/infrastructure/redis/storage/statistic_storage.py
@@ -8,24 +8,23 @@ class StatisticStorage(RedisStorage):
     def __init__(self):
         super().__init__(prefix="statistic")
 
-    def set_daily_messages(self, timestamp: int, value: int, key: str = "daily_messages"):
-
-        self.set_value(f"{key}:{timestamp}",value, expiration=60 * 60 * 24 * 7)
+    def set_daily_messages(self, timestamp: int, value: int, key: str = "daily_messages", days: int = 7):
+        self.set_value(f"{key}:{timestamp}", value, expiration=60 * 60 * 24 * days)
 
     def get_all_daily_messages(self, key: str = "daily_messages"):
         ans = {}
         keys = self.keys(f"{key}:*")
         for k in sorted(keys):
-            timestamp = k[k.rfind(":")+1:]
+            timestamp = k[k.rfind(":") + 1:]
             dt = datetime.fromtimestamp(float(timestamp))
 
             # Форматируем вывод по своему желанию
             formatted_date = dt.strftime("%Y-%m-%d")
-            ans[formatted_date]=self.get_value(key = k[1:])
+            ans[formatted_date] = self.get_value(key=k[1:])
         return ans
 
-    def set_day_active_chats(self, timestamp: int, value: int, key: str = "day_active_chats"):
-        self.set_value(f"{key}:{timestamp}",value, expiration=60 * 60 * 24 * 7)
+    def set_day_active_chats(self, timestamp: int, value: int, key: str = "day_active_chats", days: int = 7):
+        self.set_value(f"{key}:{timestamp}", value, expiration=60 * 60 * 24 * days)
 
     def get_all_day_active_chats(self, key: str = "day_active_chats"):
         ans = {}
